dd70b8becb315dec89c17e03c7c1e46fdc37f57d
Nem Simeunovic <nemanja@trunkclub.com>
2015-09-10 13:29:07 -0500

    installing ag as part of bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git rake
+apt-get install -y git rake silversearcher-ag
 
 git clone https://github.com/NemSim/dotfiles /home/vagrant/dotfiles
+# FIXME chown dotfiles directory to the vagrant user
+
 # FIXME auto set up dotfiles
 # cd /home/vagrant/dotfiles && rake install['/home/vagrant/']
