d2f242e4b19e3ed00814443fff8c972db711278e
Nem Simeunovic <nemanja@trunkclub.com>
2015-09-12 18:01:17 -0500

    add some stuff to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git rake silversearcher-ag
+apt-get install -y zsh git rake silversearcher-ag libffi-dev libssl-dev libreadline-dev zlib1g-dev
 
 git clone https://github.com/NemSim/dotfiles /home/vagrant/dotfiles
-# FIXME chown dotfiles directory to the vagrant user
 
-# FIXME auto set up dotfiles
+git clone https://github.com/rupa/z /home/vagrant/tools/z
+
+git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+
+# FIXME set up zsh by default
+
+chown -R vagrant:vagrant /home/vagrant/tools
+chown -R vagrant:vagrant /home/vagrant/.rbenv
+chown -R vagrant:vagrant /home/vagrant/dotfiles
+
+# FIXME auto set up dotfiles by rake like:
 # cd /home/vagrant/dotfiles && rake install['/home/vagrant/']
